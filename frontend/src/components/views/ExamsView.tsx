d56ade0a3eb236e5a9515d9f1b999b3c87c262b8
BastianChinchon <bastian.chinchon@forcast.cl>
2023-05-15 21:02:39 -0400

    Se implementó el bloqueo de examenes

--- frontend/src/components/views/ExamsView.tsx
+++ frontend/src/components/views/ExamsView.tsx
@@ -9,9 +9,11 @@ import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 import {
   getExam,
   getExamPredictedMarkersComputations,
+  markExamIdAsLocked,
+  markExamIdAsUnlocked,
   putExamReview, putExamUnreview
 } from "../../service/user.service";
-import { useParams } from "react-router-dom";
+import { NavigateFunction, useNavigate, useParams } from "react-router-dom";
 
 import { useTranslation } from "react-i18next";
 import DiagnosisComponent from "../customComponents/DiagnosisComponent";
@@ -53,6 +55,63 @@ const ExamsView: React.FC<ExamsViewProps> = ({
   const examIdNumber = parseInt(examId || "0", 10);
   const [examData, setExamData] = useState<ExamData>();
   const [validated, setValidated] = useState<boolean>();
+  const [isLocked, setIsLocked] = useState<boolean>();
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [reloadTime, setReloadTime] = useState<number>();
+
+  useEffect(
+    () => {
+      //llamar función que hace la request para bloquear posiblemente con algún setTimeout para que se repita cada cierto tiempo
+      markExamIdAsLocked(examIdNumber).then(
+        (response) => {
+          setIsLocked(false);
+          console.log("RESPONSE LOCKED = ", response);
+        },
+        (error) => {
+          if(error.response.status === 423){
+            console.log("RESPONSE ERROR = ", error.response.data);
+            setIsLocked(true);
+            const timeRemaining = error.response.data.timeRemaining;
+            const expTime = new Date(timeRemaining);
+            const asTimestamp = new Date(error.response.data.asTimestamp);
+            const msg = `Este examen está siendo visualizado por ${error.response.data.lockedBy}, el bloqueo expirará a las ${expTime.toLocaleTimeString()} a menos que 
+            ${error.response.data.lockedBy} lo prolongue o lo deje de visualizar.`
+            setErrorMessage(msg);
+            setReloadTime(asTimestamp.getTime());
+            console.log("RELOAD TIME = ", reloadTime);
+          }
+        }
+      )
+      return () => {
+        //llamar función que hace la request para desbloquear
+        markExamIdAsUnlocked(examIdNumber).then(
+          (response) => {
+            console.log("RESPONSE UNLOCKED: ", response);
+          },
+          (error) => {
+            console.error('Error al liberar el bloqueo:', error);
+          }
+        );
+      }
+  }, []);
+  
+  useEffect(() => {
+    if(isLocked){
+      const timeoutId = setTimeout(()=>{
+        navigate(0);
+      }, reloadTime);
+      console.log("Se setea el timeout con id = ", timeoutId);
+      return () => clearTimeout(timeoutId);
+    } else{
+      const intervalId = setInterval(()=>{
+        markExamIdAsLocked(examIdNumber);
+      }, 55000);
+      console.log("Se setea el interval con id = ", intervalId);
+
+      return () => clearInterval(intervalId);
+    }
+
+  }, [isLocked]);
 
   useEffect(() => {
     getExam(examIdNumber).then(
@@ -113,7 +172,152 @@ const ExamsView: React.FC<ExamsViewProps> = ({
   
   const isMatchXs = useMediaQuery(theme.breakpoints.down("md"));
 
-  if (isMatchXs){
+  const navigate: NavigateFunction = useNavigate();
+
+  
+  const handleGoBack = (): void => {
+    markExamIdAsUnlocked(examIdNumber).then(
+      (response) => {
+        console.log("RESPONSE UNLOCKED: ", response);
+        setIsLocked(false);
+      },
+      (error) => {
+        console.error('Error al liberar el bloqueo:', error);
+      }
+    );
+    navigate("/exams");
+  };
+
+  if (isLocked && !isMatchXs){
+    return(
+      <>
+        <Header
+          tabs={tabs}
+          buttons={buttons}
+          headerPositionLg="sticky"
+          headerPositionMd="sticky"
+          headerPositionXs="sticky"
+          onTabValueChange={(index: number) => {
+            console.log(`Exams: Tab index changed to ${index}`);
+          }}
+        />
+        {/* Grid que contiene todo lo de la vista del examen */}
+        <Grid container marginY={"1%"} width={"100%"} 
+          >
+          {/* Contenedor del boton para volver a la tabla de exámenes*/}
+          <Grid item xs={6} sm={6} md={2} lg={2} width={"80%"} >
+            <ThemeProvider theme={buttonsTheme}>
+              <Button
+              variant="contained"
+              onClick={handleGoBack}
+              sx={{
+                position: "fixed",
+                left: "2%",
+                width: "10%",
+                backgroundColor: '#007088',
+                color: "#fff", // Set a fixed width for the button, // Use shorthand notation for marginLeft
+              }}
+              >
+                <Typography color="#ffffff" fontWeight={"bold"} >
+                  {t("goBack")}
+                </Typography>
+              </Button>
+            </ThemeProvider>
+          </Grid>
+
+        {/* Grid que contiene la parte de la informacion del examen */}
+        <Grid container display={"flex"}  mb={"4%"} item xs={12} sm={12} md={8} lg={8} flexDirection={"column"}>
+          {/* Grid que contiene la información del examen (incluyendo el análisis de este) */}
+          <Grid container display={"flex"} justifyContent={"space-around"} >
+            <Grid container xs={12} sm={12} md={5} lg={5} padding={"2%"}
+              sx={{
+                border: 4,
+                borderColor: "#E4EDEF",
+                borderRadius: "1%",
+                boxShadow: "0px 4px 8px rgba(0,0,0,0.3)",
+                transition: "box-shadow 0.3s ease-in-out",
+                "&:hover": {
+                  boxShadow: "0px 8px 16px rgba(0,0,0,0.3)",
+                },
+              }}
+            >
+            {errorMessage}
+            </Grid>
+            </Grid>
+            </Grid>
+
+      </Grid>
+      <div style={{width: "100%", bottom: 0, position: "relative"}}>
+      <Footer 
+        footerPositionLg="sticky"
+        footerPositionMd="sticky"
+        footerPositionXs="sticky" />
+        </div>
+      </>
+      )
+  }
+  else if (isLocked && isMatchXs){
+    return(
+      <>
+        <Header
+          tabs={tabs}
+          buttons={buttons}
+          headerPositionLg="relative"
+          headerPositionMd="relative"
+          headerPositionXs="sticky"
+          onTabValueChange={(index: number) => {
+            console.log(`Exams: Tab index changed to ${index}`);
+          }}
+        />
+        {/* Grid que contiene todo lo de la vista del examen */}
+        <Grid container marginY={"1%"} width={"100%"} display={"flex"} alignItems={"center"}
+          >
+          {/* Contenedor del boton para volver a la tabla de exámenes*/}
+          <Grid item xs={6} sm={6} md={2} lg={2} width={"80%"} marginBottom={"1%"}>
+            <ThemeProvider theme={buttonsTheme}>
+              <Button
+              variant="contained"
+              onClick={handleGoBack}
+              sx={{
+                backgroundColor: '#007088',
+                color: "#fff",
+                width: "80%", // Set a fixed width for the button, // Use shorthand notation for marginLeft
+              }}
+              >
+                <Typography color="#ffffff" fontWeight={"bold"} >
+                  {t("goBack")}
+                </Typography>
+              </Button>
+            </ThemeProvider>
+          </Grid>
+
+        {/* Grid que contiene la parte de la informacion del examen */}
+        <Grid container display={"flex"}  mb={"4%"} item xs={12} sm={12} md={8} lg={8} flexDirection={"column"}>
+          {/* Grid que contiene la información del examen (incluyendo el análisis de este) */}
+          <Grid container display={"flex"} justifyContent={"space-around"} >
+            <Grid container xs={12} sm={12} md={5} lg={5} padding={"2%"}
+              sx={{
+                border: 4,
+                borderColor: "#E4EDEF",
+                borderRadius: "1%",
+                boxShadow: "0px 4px 8px rgba(0,0,0,0.3)",
+                transition: "box-shadow 0.3s ease-in-out",
+                "&:hover": {
+                  boxShadow: "0px 8px 16px rgba(0,0,0,0.3)",
+                },
+              }}
+            >
+            {errorMessage}
+            </Grid>
+            </Grid>
+            </Grid>
+
+      </Grid>
+      </>
+      )
+  }
+
+  else if (isMatchXs){
   return (
     <>
       <Header
@@ -134,7 +338,7 @@ const ExamsView: React.FC<ExamsViewProps> = ({
         <ThemeProvider theme={buttonsTheme}>
           <Button
           variant="contained"
-          href="/exams"
+          onClick={handleGoBack}
           sx={{
             backgroundColor: '#007088',
             color: "#fff",
@@ -287,7 +491,7 @@ const ExamsView: React.FC<ExamsViewProps> = ({
         <ThemeProvider theme={buttonsTheme}>
           <Button
           variant="contained"
-          href="/exams"
+          onClick={handleGoBack}
           sx={{
             position: "fixed",
             left: "2%",
