0bb78b8dd00d3334c7d7ecef9f2aae5f71e44a0d
Viaktruli <vakel@uc.cl>
2023-06-07 10:25:23 -0400

    Merge remote-tracking branch 'origin/escala-graficos-derivaciones' into escala-graficos-derivaciones

--- frontend/src/components/customComponents/FiducialChart.tsx
+++ frontend/src/components/customComponents/FiducialChart.tsx
@@ -102,8 +102,6 @@ const FiducialChart = (props: any): JSX.Element => {
 
 
   function urgencyColorSwitcher(value: number | undefined): string {
-    console.log(value);
-    console.log("LOL!");
     switch (value) {
       case undefined:
         return "black";
@@ -210,7 +208,6 @@ useEffect(() => {
 useEffect(() => {
   getExam(examId).then(      
     (response) => {
-      console.log(response.data);
       setLineColor(urgencyColorSwitcher(response.data.urgency))
     }
   );
