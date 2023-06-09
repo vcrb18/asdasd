04e832066a1d3a901dbc94ff53750af596a0e883
Ignacio Porte <ignacio.porte@uc.cl>
2023-06-09 13:02:08 -0400

    chore(lint): add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,10 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -11,4 +13,14 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+      }
+    },
+  },
+};
