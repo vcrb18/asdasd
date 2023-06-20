3fa48d50bdb6f20ed95e76b415e8ce351d9e58ba
BastianChinchon <bastian.chinchon@forcast.cl>
2023-06-20 15:08:56 -0400

    Fix: Header implementation changed.

--- src/components/landing/ResponsiveAppBar.tsx
+++ src/components/landing/ResponsiveAppBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { useAuth } from '@/hooks/AuthContext';
 
 import { 
@@ -11,126 +11,209 @@ import {
   ListItemButton, 
   ListItemIcon, 
   ListItemText, 
-  Toolbar, 
-  Typography, 
-  useMediaQuery, 
-  useTheme } from '@mui/material';
+  MenuItem, 
+  Select, 
+  SelectChangeEvent, 
+  Typography} from '@mui/material';
 
 import MenuIcon from "@mui/icons-material/Menu";
 import LogoImage from '@/assets/images/logo_isatec.png';
+import { useTranslation } from 'react-i18next';
+
+function DrawerButton({
+  key,
+  onClick,
+  href,
+  label
+}: {
+  key: string;
+  onClick: (() => void) | undefined;
+  href: string;
+  label: string;
+}) {
+  return(
+    <ListItemButton
+    key={key}
+    onClick={onClick}
+    href={href}
+  >
+    <ListItemIcon>
+      <ListItemText> {label} </ListItemText>
+    </ListItemIcon>
+  </ListItemButton>
+  );
+}
+
+function DrawerButtons() {
+  const { i18n } = useTranslation();
+  const { user, signOut } = useAuth();
+
+  const [language, setLanguage] = useState<string>(i18n.language);
+
+  const handleLanguageChange = useCallback((event: SelectChangeEvent): void => {
+    const newLanguage = event.target.value;
+    setLanguage(newLanguage);
+    i18n.changeLanguage(newLanguage).catch((error) => {
+      console.error(error);
+    });
+  }, [i18n]);
+  return (
+  <>
+    <Select
+      value={language}
+      onChange={handleLanguageChange}
+      size="small"
+      sx={{
+        width: "154px",
+        color: "#00000089",
+      }}
+    >
+      <MenuItem value="es"> Spanish </MenuItem>
+      <MenuItem value="en"> English </MenuItem>
+    </Select>
+    {user ? (
+    <List>
+      <DrawerButton key="logout"  onClick={signOut} href={""} label="Log out"/>
+    </List> 
+    ) : (
+    <List>
+      <DrawerButton key="login" onClick={undefined} href={"/login"} label="Log in"/>
+      <DrawerButton key="register" onClick={undefined} href={"/register"} label="Register"/>
+  </List> 
+  )}
+  </>
+  );
+}
+
+function Buttons({
+  setOpenDrawer, 
+  openDrawer
+}: { 
+  setOpenDrawer: Dispatch<SetStateAction<boolean>>; 
+  openDrawer: boolean; 
+}) {
+  const { i18n } = useTranslation();
+  const { user, signOut } = useAuth();
+
+  const [language, setLanguage] = useState<string>(i18n.language);
+
+  const handleLanguageChange = useCallback((event: SelectChangeEvent): void => {
+    const newLanguage = event.target.value;
+    setLanguage(newLanguage);
+    i18n.changeLanguage(newLanguage).catch((error) => {
+      console.error(error);
+    });
+  }, [i18n]);
+  return(
+    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+    {user ? (
+      <Box sx={{ display: 'flex', flexDirection: 'row', my: 2 }}>
+        <Typography variant="h5" sx={{ alignSelf: 'center' }} color="primary">
+          {user.email}
+        </Typography>
+        <Button
+          key="logout"
+          variant="contained"
+          onClick={signOut}
+          sx={{
+            mx: 8,
+            display:{ xs: "none", sm: "block"}
+          }}
+        >
+          Log out
+        </Button>
+      </Box>
+    ) : (
+      <Box sx={{ display: 'flex', flexDirection: 'row', my: 2 }}>
+        <Button
+          key="login"
+          href="/login"
+          variant="contained"
+          sx={{
+            mx: 4,
+            display:{ xs: "none", sm: "flex"}
+          }}
+        >
+          Log in
+        </Button>
+        <Button 
+          key="register" 
+          href="/register" 
+          variant="contained" 
+          sx={{
+            mx: 4,
+            display:{ 
+              xs: "none", 
+              sm: "flex"
+        }}}>
+          Register
+        </Button>
+      </Box>
+    )}
+    <Select
+      value={language}
+      onChange={handleLanguageChange}
+      sx={{
+        mx: 4,
+        marginTop: "17px",
+        height: "36.5px",
+        backgroundColor: "#007088",
+        color: "#fff",
+        display:{ xs: "none", sm: "flex"}
+      }}
+    >
+      <MenuItem value="es"> Spanish </MenuItem>
+      <MenuItem value="en"> English </MenuItem>
+    </Select>
+    <IconButton
+      sx={{ color: "#e0e0e0", marginLeft: "auto", display:{ xs: "flex", sm: "none"}}}
+      onClick={() => {
+        setOpenDrawer(!openDrawer);
+      }}
+    >
+      <MenuIcon sx={{ marginLeft: "auto", color: '#000'}} />
+    </IconButton>
+  </Box>
+  );
+}
 
 function ResponsiveAppBar() {
-  const { user, signOut } = useAuth();
-  
-  const theme = useTheme();
-  const isMatchMd = useMediaQuery(theme.breakpoints.down("lg"));
-  const isMatchXs = useMediaQuery(theme.breakpoints.down("md"));
-
   const [openDrawer, setOpenDrawer] = useState(false);
 
-  if (!isMatchXs && !isMatchMd){
-    return (
-      <AppBar
-        position="sticky"
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        display: 'flex',
+        bgcolor: 'grey.100',
+      }}
+    >
+      <Box
         sx={{
           display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          py: 1,
+          mx: 4,
           bgcolor: 'grey.100',
         }}
       >
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            py: 1,
-            mx: 4,
-            bgcolor: 'grey.100',
-          }}
+        <img src={LogoImage} height="48px" alt="logo" className="logo" />
+
+        <Buttons openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
+      </Box>
+      <Drawer
+        disableScrollLock={true}
+        open={openDrawer}
+        onClose={() => {
+          setOpenDrawer(false);
+        }}
+        sx={{display:{ xs: "flex", sm: "none"}}}
         >
-          <img src={LogoImage} height="48px" alt="logo" className="logo" />
-  
-          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-            {user ? (
-              <>
-                <Typography variant="h5" sx={{ alignSelf: 'center' }} color="primary">
-                  {user.email}
-                </Typography>
-                <Button
-                  key="logout"
-                  onClick={signOut}
-                  sx={{
-                    mx: 12,
-                    display: 'block',
-                  }}
-                >
-                  Log out
-                </Button>
-              </>
-            ) : (
-              <Box sx={{ display: 'flex', flexDirection: 'row', my: 2 }}>
-                <Button
-                  key="login"
-                  href="/login"
-                  variant="contained"
-                  sx={{
-                    mx: 6,
-                  }}
-                >
-                  Log in
-                </Button>
-                <Button key="register" href="/register" variant="contained">
-                  Register
-                </Button>
-              </Box>
-            )}
-          </Box>
-        </Box>
-      </AppBar>
-    );
-  } else {
-    return (
-      <AppBar
-        position="sticky"
-        sx={{ background: "#fff", height: "auto", width: "100%", top: "0" }}
-      >
-        <Toolbar>
-          <IconButton
-            sx={{ color: "#e0e0e0", marginLeft: "auto" }}
-            onClick={() => {
-              setOpenDrawer(!openDrawer);
-            }}
-          >
-            <MenuIcon sx={{ marginLeft: "auto", color: '#000'}} />
-          </IconButton>
-          <Drawer
-            open={openDrawer}
-            onClose={() => {
-              setOpenDrawer(false);
-            }}
-          >
-            <List>
-                <ListItemButton
-                  key="login"
-                  href="/login"
-                >
-                  <ListItemIcon>
-                    <ListItemText>Log in</ListItemText>
-                  </ListItemIcon>
-                </ListItemButton>
-                <ListItemButton
-                  key="register" 
-                  href="/register"
-                >
-                  <ListItemIcon>
-                    <ListItemText>Register</ListItemText>
-                  </ListItemIcon>
-                </ListItemButton>
-          </List>
-          </Drawer>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-  }
+          <DrawerButtons/>
+      </Drawer>
+    </AppBar>
+  );
+}
 export default ResponsiveAppBar;
