05515dabce9cfc90eeadb77269df403142ac8f9d
Edu1806 <ehormazabal1iin@gmail.com>
2023-08-28 14:25:24 -0400

    feat: update dashboard url for report and metrics

--- frontend/src/utils/routingPropConsts.tsx
+++ frontend/src/utils/routingPropConsts.tsx
@@ -13,16 +13,16 @@ export const loginPageButtons = [{ label: "home", href: "/information" }];
 
 export const mainMenuPageButtons = [
   { label: "exams", target: "", href: "/exams"},
-  { label: "metrics", target: "_blank", href: "/metrics"},
+  { label: "metrics", target: "_blank", href: "https://sistemed2.secure.isatec.cl/d/f72348cb-4848-45cd-9454-a15765ef954a/metricas-ia?orgId=1"},
   { label: "alerts", target: "", href: "/alerts" },
-  { label: "report", target: "_blank", href: "/reports" },
+  { label: "report", target: "_blank", href: "https://sistemed2.secure.isatec.cl/d/edf09258-09f6-4f0a-bb83-61047e1a6733/operacion-ia?orgId=1" },
 ];
 
 export const mainMenuTabs = [
   { label: "exams", target: "", href: "/exams" },
-  { label: "metrics", target: "_blank", href: "/metrics" },
+  { label: "metrics", target: "_blank", href: "https://sistemed2.secure.isatec.cl/d/f72348cb-4848-45cd-9454-a15765ef954a/metricas-ia?orgId=1" },
   { label: "alerts", target: "", href: "/alerts" },
-  { label: "report", target: "_blank", href: "/reports" },
+  { label: "report", target: "_blank", href: "https://sistemed2.secure.isatec.cl/d/edf09258-09f6-4f0a-bb83-61047e1a6733/operacion-ia?orgId=1" },
 ];
 
 export const mainMenuHeaderButtons = [
