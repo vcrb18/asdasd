40c79e8d3c0594166742bc4251f9c0e562a047bf
BastianChinchon <bastian.chinchon@forcast.cl>
2023-09-06 14:09:59 -0300

    fix: fixed bug in format

--- frontend/src/components/customComponents/exams.tsx
+++ frontend/src/components/customComponents/exams.tsx
@@ -4,12 +4,12 @@ import ExamsTabGroupB from "../specialExamTable/ExamsTabGroupB";
 
 function Exams() {
   const user = getCurrentUser();
-	const isGroupB = user.UserGroups.find((userGroup: { groupId: string; }) => userGroup.groupId === 'B');
-	return (
-		<>
-			{isGroupB ? <ExamsTabGroupB /> : <ExamsTab />}
-		</>
-	);
+  const isGroupB = user.UserGroups.find((userGroup: { groupId: string; }) => userGroup.groupId === 'B');
+  return (
+	<>
+	  {isGroupB ? <ExamsTabGroupB /> : <ExamsTab />}
+	</>
+  );
 };
 
 export default Exams;
