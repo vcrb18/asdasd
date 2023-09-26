6a2f26e57c06d3580c9b171a2492037fe925cb38
Valentina Córdova <avcordova@uc.cl>
2023-09-26 15:30:55 -0300

    style(InputField): lint changes

--- src/atoms/InputField.tsx
+++ src/atoms/InputField.tsx
@@ -1,20 +1,14 @@
+import { useTranslation } from 'react-i18next';
+
 import { TextField } from '@mui/material';
 
 import { getStyle } from '@/utils/styleAuxFunctions';
 
 import { IInputFieldProps } from '@/ts/interfaces/InputFieldProps';
-import { useTranslation } from 'react-i18next';
 
 export default function InputField(props: IInputFieldProps) {
   const { t } = useTranslation();
   const { styles, label, helperText, ...rest } = props;
   const params = getStyle(styles);
-  return(
-    <TextField 
-      label={t(label)}
-      helperText={t(helperText? helperText : "")}
-      {...rest} 
-      {...params} 
-    />
-  ); 
+  return <TextField label={t(label)} helperText={t(helperText ? helperText : '')} {...rest} {...params} />;
 }
